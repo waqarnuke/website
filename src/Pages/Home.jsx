0976f09d9a4588a8654413164fdaa0e222c84ae2
waqarnuke <wnukedev@gmail.com>
2025-07-31 01:39:06 -0500

    update code

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,11 @@
-import React from 'react';
 import Header from '../Component/Header';
 import Contant from '../Component/Contant';
-import ClientSection from '../Component/ClientSection';
+
 const Home = () => {
   return (
     <>
     <Header/> 
     <Contant/>
-    {/* <ClientSection /> */}
     </>
   )
 }
